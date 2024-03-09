51b1a851a575a90e65be968fa80d5b38be0a0d45
Asrin Yagmur <auyagmur@hotmail.com>
2024-03-09 21:33:35 +0300

    auth modal store created

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,7 +10,9 @@
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
           <li>
-            <a class="px-2 text-white" href="#">Login / Register</a>
+            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
+              >Login / Register</a
+            >
           </li>
           <li>
             <a class="px-2 text-white" href="#">Manage</a>
@@ -20,3 +22,20 @@
     </nav>
   </header>
 </template>
+
+<script>
+import useModalStore from '@/stores/modal'
+import { mapStores } from 'pinia'
+
+export default {
+  name: 'Header',
+  computed: {
+    ...mapStores(useModalStore)
+  },
+  methods: {
+    toggleAuthModal() {
+      this.modalStore.isOpen = !this.modalStore.isOpen
+    }
+  }
+}
+</script>
